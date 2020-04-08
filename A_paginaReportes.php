c4745c7550d206c54dcb64e4fd0cf5d6472b374e
Scastrosa <42383259+Scastrosa@users.noreply.github.com>
2020-04-07 21:03:06 -0500

    graficas

---
+++ A_paginaReportes.php
@@ -0,0 +1,242 @@
+<!--
+
+=========================================================
+* Now UI Dashboard - v1.5.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/now-ui-dashboard
+* Copyright 2019 Creative Tim (http://www.creative-tim.com)
+
+* Designed by www.invisionapp.com Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    ADMINISTRADOR LOCAL
+  </title>
+    <?php 
+    require_once "scripts.php";
+    require_once "clases/conexion.php";
+    include_once 'controlador/user.php';
+    include_once 'controlador/user_Sesion.php';
+    
+    $obj=new conectar();
+    $conexion=$obj->conexion();
+    
+    $userSession = new UserSession();
+    $user = new Usuario();
+    
+    if(!isset($_SESSION['user']))
+    {
+        header("location: index.php");
+    }
+    
+    $user->setUser($userSession->getCurrentUser());
+    $cod_mul = $user->getCodigoMul();
+    $cod_usuario = $user->getCodUsuario();
+    $nom_c_empleado = $user->getNomTUsuario();
+    $host= gethostname();
+    $ip = gethostbyname($host);
+    
+    $sql = "insert into AUDITORIA (cod_usuario, nombre_cargo_empleado, accion, nombre_tabla, fecha_modificacion, ip_modificacion) values (".$cod_usuario.", '".$nom_c_empleado."', 'Read', 'Salas', now(),'".$ip."');";
+    $result=mysqli_query($conexion,$sql);
+    ?>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+ 
+</head>
+
+<body class="">
+  <div class="wrapper ">
+    <div class="sidebar" data-color="orange">
+      <!--
+        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+    -->
+    <div class="logo">
+        <a href="A_paginaIndex.php" class="simple-text logo-mini">
+          CJ
+        </a>
+        <a href="A_paginaIndex.php" class="simple-text logo-normal">
+          CINE JUNGLA APP
+        </a>
+      </div>
+      <div class="sidebar-wrapper" id="sidebar-wrapper">
+        <ul class="nav">
+          <li>
+            <a href="A_paginaFunciones.php">
+              <i class="fas fa-film"></i>
+              <p>GESTI&Oacute;N DE FUNCIONES</p>
+            </a>
+          </li>
+          <li>
+            <a href="A_paginaSnacks.php">
+              <i class="fas fa-hotdog">
+              </i>
+              <p>GESTI&Oacute;N DE SNACKS</p>
+            </a>
+          </li>
+          <li>
+            <a href="A_paginaSalas.php">
+              <i class="fas fa-couch"></i>
+              <p>GESTI&Oacute;N DE SALAS</p>
+            </a>
+          </li>
+          <li>
+            <a href="A_paginaEmpleados.php">
+              <i class="now-ui-icons users_single-02"></i>
+              <p>GESTI&Oacute;N DE EMPLEADOS</p>
+            </a>
+          </li>
+          <li>
+            <a href="A_paginaAuditoria.php">
+              <i class="now-ui-icons ui-1_bell-53"></i>
+              <p>AUDITORIA</p>
+            </a>
+          </li>
+          <li>
+            <a href="A_paginaReportes.php">
+              <i class="fas fa-book"></i>
+              <p>REPORTES</p>
+            </a>
+          </li>
+
+
+        </ul>
+      </div>
+    </div>
+    <div class="main-panel" id="main-panel">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
+        <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <div class="navbar-toggle">
+              <button type="button" class="navbar-toggler">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+              </button>
+            </div>
+            <a class="navbar-brand" style="font-size:3 ">
+            <?php 
+                $conexion=$obj->conexion();
+                $sql="SELECT nom_multiplex FROM MULTIPLEX where MULTIPLEX.cod_multiplex = $cod_mul";
+                $result=mysqli_query($conexion,$sql);
+                
+                while($row = $result->fetch_assoc())
+                {
+                    echo "MULTIPLEX ". $row['nom_multiplex'];
+                }
+                ?>
+          </a>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navigation">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#sergio">
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="now-ui-icons users_single-02"></i>
+                  <p>
+                    <span class="d-lg-none d-md-block">Usuario</span>
+                  </p>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="controlador/logout.php">Cerrar sesi&oacute;n</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <!-- End Navbar -->
+
+      <div>
+      <div class="content">
+        <div class="container-fluid ">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="card text-left">
+              <div class="card-header">
+                GRAFICA REPORTES
+              </div>
+              <div class="card-body">
+               <button type="button" class="btn btn-info" onclick="cargar('Snacks')"> Reportes Snacks </button>
+                <button type="button" class="btn btn-info" onclick="window.location='A_graficasReportes/A_graficaPeliculas.php'"> Reportes Peliculas </button>
+                <button type="button" class="btn btn-info" onclick="window.location='A_graficasReportes/A_graficaVentas.php'"> Reportes Ventas </button>
+
+                <hr>
+                <div id="tabladatatable">
+
+
+
+                </div>
+
+
+              </hr>
+            </div>
+            <div class="card-footer text-muted">
+              BY CINEJUNGLA APP
+            </div>
+          </div>
+        </div>
+
+      </div>
+      </div>
+      <footer class="footer">
+        <div class=" container-fluid ">
+          <nav>
+            <ul>
+              <li>
+                <a href="https://www.creative-tim.com">
+                  GRUPO DE DESARROLLO UEB SOFTWARE
+                </a>
+              </li>
+
+              <li>
+                <a href="http://blog.creative-tim.com">
+                  Blog
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div class="copyright" id="copyright">
+            &copy; <script>
+              document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+            </script>, Designed by UEBGRUPOB. Coded by <a href="https://www.creative-tim.com" target="_blank">EQUPIO COMPLETO</a>.
+          </div>
+        </div>
+      </footer>
+    </div>
+    </div>
+  </div>
+</body>
+
+</html>
+
+<script type="text/javascript">
+  $(document).ready(function(){
+    $('#tabladatatable').load('A_graficaSnacks.php');
+  });
+
+  function cargar(){
+	  $('#tabladatatable').load('A_graficaSnacks.php');
+	  };
+</script>
